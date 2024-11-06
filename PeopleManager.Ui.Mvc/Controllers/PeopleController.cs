00ab7778a55367ec582e660b8906ff6250593bfb
Sam Bonte <sam.bonte@student.vives.be>
2024-11-06 10:45:36 +0100

    Added edit CRUD functionality

--- PeopleManager.Ui.Mvc/Controllers/PeopleController.cs
+++ PeopleManager.Ui.Mvc/Controllers/PeopleController.cs
@@ -31,8 +31,45 @@ namespace PeopleManager.Ui.Mvc.Controllers
             return View();
         }
 
+        [HttpGet]
+        public IActionResult Edit(int id)
+        {
+            // FirstOrDefault() gaat er van uit dat id kan bestaan of niet
+            var person = _peopleManagerDbContext.People.FirstOrDefault(p => p.Id == id);
+            if (person is null)
+            {
+                return RedirectToAction("Index");
+            }
+            
+            return View(person);
+        }
+
         [HttpPost, ValidateAntiForgeryToken]
-        public IActionResult Create(Person person)
+        public IActionResult Edit([FromRoute]int id, [FromForm]Person person)
+        // in Controller hebben we Routing ingesteld: "{controller=Home}/{action=Index}/{id?}" 
+        // ook krijgt hij de url binnen met Home/Edit/{id}
+        // Beter om te scheiden voor duidelijkheid = veiligheid. Id = om te zoeken, person = blokje met veranderingen
+
+        {
+            var dbPerson = _peopleManagerDbContext.People.FirstOrDefault(p => p.Id == id);
+            if (dbPerson is null)
+            {
+                return RedirectToAction("Index");
+            }
+
+            // EntityFramework gaat kijken wat er moet veranderd worden en ENKEL dat loggen als changed
+            // enkel de dbPerson heeft tracking om veranderingen op te merken en toe te passen op database
+            dbPerson.FirstName = person.FirstName;
+            dbPerson.LastName = person.LastName;
+            dbPerson.Email = person.Email;
+            // Ons werk opslaan
+            _peopleManagerDbContext.SaveChanges();
+            // Na aanpassing terug naar Index pagina
+            return RedirectToAction("Index");
+        }
+
+        [HttpPost, ValidateAntiForgeryToken]
+        public IActionResult Create([FromForm]Person person) 
         {
             /*Om nieuwe persoon toe te voegen*/
             _peopleManagerDbContext.People.Add(person);
